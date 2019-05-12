3e1d4dd8b2cf988eb4598d254f5c20d0c59aae18
Nejc Zajc <nz3928@student.uni-lj.si>
2019-05-13 00:17:10 +0200

    Vrnil 31 nazaj na pravo

--- Euler_vol2.py
+++ Euler_vol2.py
@@ -243,25 +243,18 @@ def gcd(m, n):
 
 def naloga31(vrednost, seznam):
     '''How many different ways can vrednost be made using any number of coins?'''
-    resitve = {}
-    def nal31_memo(vrednost, seznam):
-        if seznam == []:
-            return 0
-        vrednost -= seznam[0]
-        if vrednost == 0:
-            vrednost += seznam[0]
-            resitve[(vrednost, seznam)] =  1 + nal31_memo(vrednost, seznam[1:])
-            return resitve[(vrednost, seznam)]
-        elif vrednost < 0:
-            vrednost += seznam[0]
-            resitve[(vrednost, seznam)] =  nal31_memo(vrednost, seznam[1:])
-            return resitve[(vrednost, seznam)]
-        elif vrednost > 0:
-            resitve[(vrednost, seznam)] = nal31_memo(vrednost, seznam) + nal31_memo(vrednost + seznam[0], seznam[1:])
-            return resitve[(vrednost, seznam)]
-    return nal31_memo(vrednost, seznam)
-#naloga31(6, [5,4,3,2,1])
-#73682
+    if seznam == []:
+        return 0
+    vrednost -= seznam[0]
+    if vrednost == 0:
+        vrednost += seznam[0]
+        return 1 + naloga31(vrednost, seznam[1:])
+    elif vrednost < 0:
+        vrednost += seznam[0]
+        return naloga31(vrednost, seznam[1:])
+    elif vrednost > 0:
+        return naloga31(vrednost, seznam) + naloga31(vrednost + seznam[0], seznam[1:])
+
 def naloga32():
     '''Find the sum of all products whose multiplicand/multiplier/product identity can be written as a 1 through 9 pandigital.'''
     mnozica_resitev = set()
@@ -734,7 +727,7 @@ def naloga76(n):
             moznosti[j] += moznosti[j - i]
     return moznosti[n]
 
-naloga76(3)
+
 # 2: 11
 # 3: 21, 111
 # 4: 31, 22, 211, 111
